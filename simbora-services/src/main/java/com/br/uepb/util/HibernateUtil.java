eb637bb509ca707fb58a96d2e15e6fc7a32bf500
brunojclementino <brunojclementino@gmail.com>
2015-05-06 09:14:38 -0300

    Classe HibernateUtil
    Classe com as atualizações dos métodos e classes importadas

--- simbora-services/src/main/java/com/br/uepb/util/HibernateUtil.java
+++ simbora-services/src/main/java/com/br/uepb/util/HibernateUtil.java
@@ -1,35 +1,26 @@
 package com.br.uepb.util;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 
 public class HibernateUtil {
 
 	private static SessionFactory sessionFactory;
+	private static ServiceRegistry serviceRegistry;
 
 	private HibernateUtil() {
 	}
 
 	public static SessionFactory getSessionFactory() {
-		if (sessionFactory == null) {
-			try {
-				// Create the SessionFactory from standard (hibernate.cfg.xml)
-				// config file.
-				/*AnnotationConfiguration ac = new AnnotationConfiguration();
-				ac.addAnnotatedClass(Usuario.class);
-				sessionFactory = ac.configure().buildSessionFactory();*/
-				sessionFactory = new AnnotationConfiguration()
-                .configure().buildSessionFactory();
-			} catch (Throwable ex) {
-				// Log the exception.
-				System.err.println("Initial SessionFactory creation failed." + ex);
-				throw new ExceptionInInitializerError(ex);
-			}
-			return sessionFactory;
-		} else {
-			return sessionFactory;
-		}
+		Configuration configuration = new Configuration();
+	    configuration.configure();
+	    serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+	            configuration.getProperties()). build();
+	    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	    return sessionFactory;
 	}
 	
 	public static void closedSession(){
